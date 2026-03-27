7a422dbb7b1bc2546de7b1258740ffc8cd4329c6
vitor-felipee <vitor07.felipe@gmail.com>
2026-03-26 23:59:00 -0300

    feat: cria servico de API e estabelece conexao com WeatherAPI

--- src/main.js
+++ src/main.js
@@ -1,2 +1,13 @@
-// Atmos360 Weather App — Ponto de entrada principal
-// Arquivo limpo e pronto para receber a lógica da aplicação.
+import { pesquisarCidade } from './services/api.js';
+
+async function initApp() {
+  try {
+    console.log('Chamando a Api...');
+    const clima = await pesquisarCidade('Curitiba');
+    console.log('Dados da API:', clima);
+  } catch (erro) {
+    console.log('Deu erro:', erro.message);
+  }
+}
+
+initApp();
